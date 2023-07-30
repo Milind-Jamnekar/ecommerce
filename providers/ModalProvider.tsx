2e08d037bbedf02d98ec58c3e48e6e4388b43cb2
Milind Jamnekar <milind.jamnekar@neomindlab.de>
2023-07-30 18:07:43 +0530

    provider added for modals

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import StoreModal from "@/components/modals/StoreModal";
+import Modal from "@/components/ui/Modal";
+import { useEffect, useState } from "react";
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    if (!isMounted) {
+      setIsMounted(true);
+    }
+  }, [isMounted, setIsMounted]);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <StoreModal />
+    </>
+  );
+};
+
+export default ModalProvider;
