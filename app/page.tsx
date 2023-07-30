0cdac25c5330b6b4398d359190c2d1edb1cecb40
Milind Jamnekar <milind.jamnekar@neomindlab.de>
2023-07-30 18:08:31 +0530

    integrate

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+import Modal from "@/components/ui/Modal";
+import { useStoreModalStore } from "@/hooks/useStoreModal";
 import { UserButton } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const onOpen = useStoreModalStore().onOpen;
+  const isOpen = useStoreModalStore().isOpen;
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
     <div className="p-4">
       <UserButton afterSignOutUrl="/" />
