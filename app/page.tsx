8e5fe3c8ddb947226805a1b099ed942bb384a3ee
Milind Jamnekar <milind.jamnekar@neomindlab.de>
2023-07-30 12:00:38 +0530

    authentication added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <Button variant="ghost">Sybmtiljdf</Button>;
+  return (
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
